1e79c3cdd3b0cf71c9185bd92fd8342a9653b580
rkmr039 <rkmr039@gmail.com>
2019-08-26 17:55:07 +0530

    .

---
+++ EMS/src/com/hcl/ems/Employ.java
@@ -0,0 +1,20 @@
+package com.hcl.ems;
+
+public class Employ {
+	private int EMP_ID;
+	private String pwd;
+	public int getEMP_ID() {
+		return EMP_ID;
+	}
+	public void setEMP_ID(int eMP_ID) {
+		EMP_ID = eMP_ID;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	
+	
+}
